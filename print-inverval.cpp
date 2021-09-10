aa0c13268e1c9156b164f74c643492993ccfd385
Jonathan Shnayder <rangersps42@gmail.com>
2021-09-10 14:35:50 -0400

    Added task B

---
+++ print-inverval.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+	int L;
+	int U;
+
+	std::cout << "Please enter L: ";
+	std::cin >> L;
+
+	std::cout << "Please enter U: ";
+	std::cin >> U;
+
+	for(int i = L; i < U; i++) {
+		std::cout << i << " ";
+	}	
+	return 0;
+
+}
