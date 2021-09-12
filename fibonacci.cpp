4dc5f76f1db9f89fc701b567f4500bcaa97657b2
Jonathan Shnayder <rangersps42@gmail.com>
2021-09-11 21:54:50 -0400

    Added task D

---
+++ fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+	int fib [60];
+	
+	fib[0] = 0;
+	fib[1] = 1;
+	
+	for(int i = 2; i < 60; i++) {
+		fib[i] = fib[i - 1] + fib[ i - 2];
+	}
+	
+	for(int i = 0; i< 60; i++) {
+		std::cout << fib[i] << std::endl;
+	}	
+
+	//The sequence starts divergeing from the actual numbers because we are using ints which have 4 bytes and can only hold numbers up to 2,147,483,647. So once you add and the number exceeds the limit the sequence breaks off form the truth and the values start wrapping back to negative values. 
+	
+	return 0;
+}
